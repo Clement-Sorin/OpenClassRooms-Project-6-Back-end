0df10333e7cfb73793a09942ba03fced1059c642
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-17 18:46:34 +0200

    server : add error management in server.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req,res) => {
-    res.json({message : "test server"})
+    res.json({message : "test server port 3100"})
 })
 
 module.exports = app;
