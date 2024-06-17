1e80253c52f139c820986a833c47e99ba1f93a39
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-17 18:21:49 +0200

    Express : installed express + create app.js + run app in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http")
+const app = require("./app")
 
-const server = http.createServer((req, res) => {
-    res.end("test server")
-})
+app.set("port", process.env.PORT || 3100)
+const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3100)
