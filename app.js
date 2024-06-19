4578d15914db531a38d2d1e5d2113cf249c0e11d
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-19 11:34:48 +0200

    API routes : create GET route for books/:id

--- app.js
+++ app.js
@@ -33,4 +33,13 @@ app.get("/api/books", async (req, res, next) => {
     }
 })
 
+app.get("/api/books/:id", async (req, res, next) => {
+    try {
+        const book = await Book.findOne({ _id: req.params.id })
+        res.status(200).json(book)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
 module.exports = app
