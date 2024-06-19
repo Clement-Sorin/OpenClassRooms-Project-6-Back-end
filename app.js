b2e1039ed880fb2ace7f44319320054847514963
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-19 10:28:31 +0200

    prettier : add automatic styling to every files

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express")
+const mongoose = require("mongoose")
 
-const app = express();
+const app = express()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    next();
-  });
-
-app.use((req,res) => {
-    res.json({message : "test server port 3100"})
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    )
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    next()
 })
 
-module.exports = app;
+app.use((req, res) => {
+    res.json({ message: "test server port 3100" })
+})
+
+module.exports = app
