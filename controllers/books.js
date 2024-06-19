b67979192c74fdebd770b774d9cf4a1a8c6b8c6c
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-19 14:12:07 +0200

    controller : create controllers folder and import all routes fonctionnality in books.js file

---
+++ controllers/books.js
@@ -0,0 +1,31 @@
+const Book = require("../models/Book")
+
+exports.getAllBooks = async (req, res, next) => {
+    try {
+        const books = await Book.find()
+        res.status(200).json(books)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+}
+
+exports.getOneBook = async (req, res, next) => {
+    try {
+        const book = await Book.findOne({ _id: req.params.id })
+        res.status(200).json(book)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+}
+
+exports.getBestRating = async (req, res, next) => {
+    try {
+        const books = await Book.find()
+        const topBooks = books
+            .sort((a, b) => b.averageRating - a.averageRating)
+            .slice(0, 3)
+        res.status(200).json(topBooks)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+}
--- routes/books.js
+++ routes/books.js
@@ -1,36 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const booksCtrl = require("../controllers/books")
 
-const Book = require("../models/Book")
-
-router.get("/", async (req, res, next) => {
-    try {
-        const books = await Book.find()
-        res.status(200).json(books)
-    } catch (error) {
-        res.status(400).json({ error })
-    }
-})
-
-router.get("/:id", async (req, res, next) => {
-    try {
-        const book = await Book.findOne({ _id: req.params.id })
-        res.status(200).json(book)
-    } catch (error) {
-        res.status(400).json({ error })
-    }
-})
-
-router.get("/bestrating", async (req, res, next) => {
-    try {
-        const books = await Book.find()
-        const topBooks = books
-            .sort((a, b) => b.averageRating - a.averageRating)
-            .slice(0, 3)
-        res.status(200).json(topBooks)
-    } catch (error) {
-        res.status(400).json({ error })
-    }
-})
+router.get("/", booksCtrl.getAllBooks)
+router.get("/:id", booksCtrl.getOneBook)
+router.get("/bestrating", booksCtrl.getBestRating)
 
 module.exports = router
