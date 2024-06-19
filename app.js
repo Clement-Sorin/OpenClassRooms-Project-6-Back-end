b9698777d63bec39e827373f050aedf703312b7d
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-19 10:57:00 +0200

    Mongoose : Set up connection to MongoDB Altas + create .env with variables for sensible datas

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
+const Book = require("./models/Book")
 
 const app = express()
+
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(() => console.log("Connection to MongoDB completed"))
+    .catch(() => console.log("Connection to MongoDB failed"))
+
+app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
